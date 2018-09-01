f107bc1a30d992e724c86c714db0cbec04b33cbb
edouard piron <ed.piron@gmail.com>
2018-09-01 11:30:14 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  resources :bags, only: %i[index show]
+  resources :items, only: %i[index show]
+
+  namespace :user do
+    get 'dashboard', to: 'user#dashboard'
+    resources :user_bags
+    resources :user_items
+    resources :user_packed_item
+    resources :user_packed_bags
+    resources :journeys
+    resources :journey_bags
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
