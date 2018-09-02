6db10480a560214b0d469c268cdf21138b16a661
edouard piron <ed.piron@gmail.com>
2018-09-02 20:49:40 +0200

    nested index items

--- app/controllers/user/items_controller.rb
+++ app/controllers/user/items_controller.rb
@@ -3,14 +3,12 @@ class User::ItemsController < ApplicationController
   before_action :set_item_ref, only: %i[create]
 
   def index
-    @items = {}
-    @user_items = current_user.items.order(item_ref_id: :asc)
-    @item_refs = @user_items.group(:item_ref_id).count
-    @item_refs.each do |item_ref_id|
-      @items[item_ref_id] = @item_refs[item_ref_id]
-    end
-    # @items_categories = ItemRef.order(category: :asc).group(:category).count
-    raise
+    @user_items = current_user.items
+    @item_refs = @user_items.map { |item| item.item_ref }.uniq
+    @item_refs.sort!
+    @categories = @item_refs.map { |item_ref| item_ref.category }.uniq
+    @categories.sort!
+    @item_refs_count = @user_items.group(:item_ref_id).count
   end
 
   def show; end
